3e4e7328853408c23f8af53591377cb17ad0193e
Nicholas Russell <narussell@fullsail.edu>
2017-05-15 15:27:57 -0400

    Fixed GUUID equivalency operator to use unions again

--- Interface/G_Core/GDefines.h
+++ Interface/G_Core/GDefines.h
@@ -24,34 +24,26 @@ namespace GW
 	*/
 	struct GUUIID // ALL GUUIID are stored little endian.
 	{
-		unsigned int	byte4;
-		unsigned short  byte2a;
-		unsigned short  byte2b;
-		unsigned char	byte8[8];
-
-		//For faster comparison.
-		//union
-		//{
-		//	unsigned long long part1;
-		//	unsigned long long part2;
-		//};
+		union
+		{
+			struct
+			{
+				unsigned int	byte4;
+				unsigned short  byte2a;
+				unsigned short  byte2b;
+				unsigned char	byte8[8];
+			};
+			//For faster comparison.
+			unsigned long long parts[2];
+		};
 
 		//! Comparison operator overload.
 		bool operator==(const GUUIID& _cmp) const
 		{
-			const unsigned long long * part1a = reinterpret_cast<const unsigned long long *>(&byte4),
-						             * part2a = reinterpret_cast<const unsigned long long *>(byte8),
-				                     * part1b = reinterpret_cast<const unsigned long long *>(&_cmp.byte4),
-				                     * part2b = reinterpret_cast<const unsigned long long *>(&_cmp.byte8);
-
-			if (*part1a != *part1b || *part2a != *part2b)
+			if (parts[0] != _cmp.parts[0] || parts[1] != _cmp.parts[1])
 				return false;
 
 			return true;
-			//if (part1 != _cmp.part1 || part2 != _cmp.part2)
-			//	return false;
-			//
-			//return true;
 		}
 	}; // Gateware guuiid mimic microsoft GUID structure in byte pattern.
 	   // Use built-in Visual Studio tools to generate unique ID for new interfaces.
