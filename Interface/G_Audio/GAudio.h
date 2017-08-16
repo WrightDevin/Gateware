580c4fe6b772d28e80bf482cbf77736e7d70c571
TJay Cargle <tbcargle@fullsail.edu>
2017-08-16 11:24:31 -0400

    Wrote all unit tests and created base implementation

--- Interface/G_Audio/GAudio.h
+++ Interface/G_Audio/GAudio.h
@@ -1,24 +1,26 @@
 #ifndef GAUDIO_H
 #define GAUDIO_H
+#endif
 /*!
 File: GAudio.h
 Purpose: A Gateware interface that handles sounds and music.
 Author: TJay Cargle
 Contributors: N/A
-Last Modified: 8/7/2017
+Last Modified: 8/15/2017
 Interface Status: Early Stage Development
 Copyright: 7thGate Software LLC.
 License: MIT
 */
 
-#include "../G_Core/GDefines.h"
+#include "GSound.h"
+#include "GMusic.h"
 
 
 //! The core namespace to which all Gateware interfaces/structures/defines must belong.
 namespace GW
 {
 	//! The namespace to which all Gateware library interfaces must belong.
-	namespace SYSTEM
+	namespace AUDIO
 	{
 
 		//! Ensure identical binary padding for structures on all platforms.
@@ -28,51 +30,82 @@ namespace GW
 
 
 
-		//! Unique Identifier for this interface. {A5D7746F-FAAE-42BF-AC16-5C25551CA7B6}.
-		static const GUUIID GWindowUUIID =
+		//! Unique Identifier for this interface. {AA479919-631C-488D-B278-DBF6D139988C}.
+		static const GUUIID GAudioUUIID =
 		{
-			0xa5d7746f, 0xfaae, 0x42bf,{ 0xac, 0x16, 0x5c, 0x25, 0x55, 0x1c, 0xa7, 0xb6 }
+
+		 0xaa479919, 0x631c, 0x488d,{ 0xb2, 0x78, 0xdb, 0xf6, 0xd1, 0x39, 0x98, 0x8c } 
+
 		};
-		class G_Sound
+
+		class GAudio
 		{
 
 		public:
-			
-			virtual GReturn PlaySound(const char* soundName) = 0;
-			virtual GReturn SetVolume(unsigned int newVolume, const char* soundName) = 0;
-			virtual GReturn Pause() = 0;
-			virtual GReturn Resume() = 0;
-			virtual GReturn SetVolumes(float *values, int numChannels) = 0;
-
-
-
-		}; // end GSound class
-		class G_Music
-		{
-
-		public:
-			virtual GReturn PlayMusic(const char* soundName) = 0;
-			virtual GReturn SetVolume(unsigned int newVolume, const char* soundName) = 0;
-			virtual GReturn Pause() = 0;
-			virtual GReturn Resume() = 0;
-			virtual GReturn SetChannelVolumes(float *values, int numChannels) = 0;
-
-
-		}; // end GMusic class
-		class G_Audio
-		{
-
-		public:
+			//! Initializes all neccassary data for GAudio based on platform.
+			/*!
+			*	On Windows, initilaizes XAudio2 pointers IXAudio2 and IXAudio2MasteringVoice.
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
 			virtual GReturn Init() = 0;
-			virtual GReturn CreateSound(const char* path, G_Sound** outSound) = 0;
-			virtual GReturn CreateMusicStream(const char* path, G_Music** outMusic) = 0;
-			virtual GReturn SetMasterVolume(float value) = 0;
-			virtual GReturn SetMasterChannelVolumes(float *values, int numChannels) = 0;
+			//! Fills out a GSound with data from provided .wav file.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
+			virtual GReturn CreateSound(const char* _path, GSound** _outSound) = 0;
+			//! Fills out a GMusic with data from provided .wav file.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
+			virtual GReturn CreateMusicStream(const char* _path, GMusic** _outMusic) = 0;
+			//! Sets Master Volume for all sounds and music.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
+			virtual GReturn SetMasterVolume(float _value) = 0;
+			//! Sets Master Volume for all sounds and music for specifc speakers.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
+			virtual GReturn SetMasterChannelVolumes(const float * _values, int _numChannels) = 0;
+			//! Sets Pauses all sounds and music.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
 			virtual GReturn PauseAll() = 0;
+			//! Sets Resumes all paused/stoped sounds and music.
+			/*!
+			*	Stoped Sounds/Music will play from the start
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
 			virtual GReturn ResumeAll() = 0;
+			//!Destructor for GAudio. Destroys all sounds and music it created.
+			/*!
+			*	tbd
+			*	\retval SUCCESS tbd
+			*   \retval REDUNDANT_OPERATION tbd
+			*/
+			//virtual ~GAudio();
 	
 
 		}; // end GAudio class
+		GATEWARE_EXPORT_IMPLICIT GReturn CreateGAudio(GAudio** _outAudio)
+		{
+			return FAILURE;
+		}
+	
 
 	} // end SYSTEM namespace
 } // end GW namespace
--- Source/G_Audio/GAudio.cpp
+++ Source/G_Audio/GAudio.cpp
@@ -1,18 +1,215 @@
 #ifdef _WIN32
-#include <Windows.h>
+#include"GAudioWin.hpp"
 #elif __linux__
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <pulse/mainloop.h>
-#include <pulse/context.h>
-#include <pulse/stream.h>
-#include <pulse/error.h>
-#include <pulse/scache.h>
-#elif __APPLE__
+#include"GAudioLinux.hpp"
+//#elif __APPLE__
+//#import "GAudioMac.hpp"
+#endif
 
-#ifdef __OBJC__
-#import <Foundation/Foundation.h>
-#import <Cocoa/Cocoa.h>
-#endif
-#endif
+using namespace GW;
+using namespace AUDIO;
+class AppAudio : public GAudio
+{
+public:
+
+	GReturn Init();
+
+	GReturn CreateSound(const char* _path, GSound** _outSound);
+
+	GReturn CreateMusicStream(const char* _path, GMusic** _outMusic);
+
+	GReturn SetMasterVolume(float _value);
+
+	GReturn SetMasterChannelVolumes(const float * _values, int _numChannels);
+
+	GReturn PauseAll();
+
+	GReturn ResumeAll();
+
+	~AppAudio();
+
+};
+//Start of GAudio implementation
+GReturn AppAudio::Init()
+{
+
+	GReturn result = PlatformInit();
+
+	return result;
+}
+GReturn AppAudio::CreateSound(const char* _path, GSound** _outSound)
+{
+	GReturn result = PlatformCreateSound(_path, _outSound);
+
+	return result;
+}
+GReturn AppAudio::CreateMusicStream(const char* _path, GMusic** _outMusic)
+{
+	GReturn result = PlatformCreateMusicStream(_path, _outMusic);
+
+	return result;
+}
+GReturn AppAudio::SetMasterVolume(float _value)
+{
+	GReturn result = PlatformSetMasterVolume(_value);
+
+	return result;
+}
+GReturn AppAudio::SetMasterChannelVolumes(const float * _values, int _numChannels)
+{
+	GReturn result = PlatformSetMasterChannelVolumes(_values, _numChannels);
+
+	return result;
+}
+GReturn AppAudio::PauseAll()
+{
+	GReturn result = PlatformPauseAll();
+
+	return result;
+}
+GReturn AppAudio::ResumeAll()
+{
+	GReturn result = PlatformResumeAll();
+
+	return result;
+}
+AppAudio::~AppAudio()
+{
+	PlatformUnloadALL();
+}
+//GATEWARE_EXPORT_EXPLICIT GReturn CreateGAudio(GAudio** _outAudio) 
+//{
+//	return FAILURE; 
+//}
+//GReturn GW::AUDIO::CreateGAudio(GAudio** _outAudio)
+//{
+//	return FAILURE;
+//}
+//End of GAudio implementation 
+
+class AppSound : public GSound
+{
+public:
+	GReturn SetPCMShader(const char* _data);
+	GReturn SetChannelVolumes(float *_values, int _numChannels);
+	GReturn SetVolume(float _newVolume);
+	GReturn PlaySound();
+	GReturn Pause();
+	GReturn Resume();
+	GReturn StopSound();
+
+	~AppSound();
+
+};
+
+//Start of GSound implementation 
+GReturn AppSound::SetPCMShader(const char* _data)
+{
+	GReturn result = PlatformSetSoundPCMShader(_data);
+
+	return result;
+}
+GReturn AppSound::SetChannelVolumes(float * _values, int _numChannels)
+{
+	GReturn result = PlatformSetSoundChannelVolumes(_values, _numChannels);
+
+	return result;
+}
+GReturn AppSound::SetVolume(float _newVolume)
+{
+	GReturn result = PlatformSetSoundVolume(_newVolume);
+
+	return result;
+}
+GReturn AppSound::PlaySound()
+{
+	GReturn result = PlatformPlaySound();
+
+	return result;
+}
+GReturn AppSound::Pause()
+{
+	GReturn result = PlatformPauseSound();
+
+	return result;
+}
+GReturn AppSound::Resume()
+{
+	GReturn result = PlatformResumeSound();
+
+	return result;
+}
+GReturn AppSound::StopSound()
+{
+	GReturn result = PlatformStopSound();
+
+	return result;
+}
+AppSound::~AppSound()
+{
+	PlatformUnloadGSound();
+}
+//End of GSound implementation 
+
+class AppMusic : public GMusic
+{
+public:
+	GReturn SetPCMShader(const char* _data);
+	GReturn SetChannelVolumes(float *_values, int _numChannels);
+	GReturn SetVolume(float _newVolume);
+	GReturn StreamStart();
+	GReturn PauseStream();
+	GReturn ResumeStream();
+	GReturn StopStream();
+
+	~AppMusic();
+
+};
+//Start of GMusic implementation 
+GReturn AppMusic::SetPCMShader(const char* _data)
+{
+	GReturn result = PlatformSetMusicPCMShader(_data);
+
+	return result;
+}
+GReturn AppMusic::SetChannelVolumes(float * _values, int _numChannels)
+{
+	GReturn result = PlatformSetMusicChannelVolumes(_values, _numChannels);
+
+	return result;
+}
+GReturn AppMusic::SetVolume(float _newVolume)
+{
+	GReturn result = PlatformSetMusicVolume(_newVolume);
+
+	return result;
+}
+GReturn AppMusic::StreamStart()
+{
+	GReturn result = PlatformStartStream();
+
+	return result;
+}
+GReturn AppMusic::PauseStream()
+{
+	GReturn result = PlatformPauseStream();
+
+	return result;
+}
+GReturn AppMusic::ResumeStream()
+{
+	GReturn result = PlatformResumeStream();
+
+	return result;
+}
+GReturn AppMusic::StopStream()
+{
+	GReturn result = PlatformStopStream();
+
+	return result;
+}
+AppMusic::~AppMusic()
+{
+	PlatformUnloadGMusic();
+}
+//End of GMusic implementation 
