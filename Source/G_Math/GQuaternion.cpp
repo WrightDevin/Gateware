c1d3f5d5c80fceb736cf3964f267811661a4059e
ShuoYI <schang@fullsail.edu>
2017-10-04 11:52:16 -0400

    Modified Source/G_Math/CMakeLists.txt

--- Source/G_Math/GQuaternion.cpp
+++ Source/G_Math/GQuaternion.cpp
@@ -1,7 +1,6 @@
 // Override export symbols for DLL builds (must be included before interface code).
 #include "../DLL_Export_Symbols.h"
 #include "../../Interface/G_Math/GQuaternion.h"
-#include "../../Interface/G_Math/GMatrix.h"
 #include "../Source/G_System/GUtility.h"
 
 #include <atomic> //Thread safety
