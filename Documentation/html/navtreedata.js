c99867e283171fa7036a8de40b23e92250c00f98
Andre Reid <AJReid@fullsail.edu>
2017-11-20 02:25:04 -0500

    Added GGraphics Documentation

---
+++ Documentation/html/navtreedata.js
@@ -0,0 +1,44 @@
+var NAVTREE =
+[
+  [ "Gateware Libraries", "index.html", [
+    [ "Installation", "index.html#Install", null ],
+    [ "API Overview", "index.html#API", [
+      [ "GLog", "index.html#APIGLog", null ],
+      [ "GFile", "index.html#APIGFile", null ],
+      [ "GInput", "index.html#APIGInput", null ],
+      [ "GBufferedInput", "index.html#APIGBufferedInput", null ]
+    ] ],
+    [ "Bug Reporting", "index.html#Reporting", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"GAudio_8h_source.html",
+"dir_b7263c2df3abed61e1897d5e9e534860.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
