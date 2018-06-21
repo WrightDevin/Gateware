4c77b9a2b5069f2a778ee9a3c5a6aa3c2437b696
TylerMurray <TWMurray@student.fullsail.edu>
2018-06-21 11:48:54 -0400

    All Good on Linux
    No problems orrcured by merge

--- Source/GVersion.hpp
+++ Source/GVersion.hpp
@@ -35,14 +35,14 @@ SOFTWARE.
 // The Minor version is increased whenever an interface is altered.(patch is reset)
 #define GATEWARE_MINOR 0
 // The Patch version is increased whenever a bug or leak is fixed.(nothing is reset)
-#define GATEWARE_PATCH 'g'
+#define GATEWARE_PATCH ''
 // Pulled directly from GIT  
-#define GATEWARE_BRANCH "ClearBuffersFeature"
+#define GATEWARE_BRANCH "Tyler"
 // Pulled directly from GIT
-#define GATEWARE_COMMIT_HASH 0x717f0fd
+#define GATEWARE_COMMIT_HASH 0x13424ed
 // Standard Window Title Bar
-#define GATEWARE_VERSION_STRING "Gateware v4.0g"
+#define GATEWARE_VERSION_STRING "Gateware v4.0"
 // Window Title Bar displayed in DEBUG builds
-#define GATEWARE_VERSION_STRING_LONG "Gateware v4.0g (ClearBuffersFeature) [717f0fd]"
+#define GATEWARE_VERSION_STRING_LONG "Gateware v4.0 (Tyler) [13424ed]"
 
 #endif
